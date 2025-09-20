43b52f12ec059381795c7eee7e7cdf606ae1a785
Arnav Singh <rinadevi2453@gmail.com>
2025-09-20 16:32:22 +0530

    loc ping

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import driverRoutes from "./routes/driver.js";
+
 const app = express();
-
-const uri = "mongodb+srv://bora:arnav@cluster0.viblu0y.mongodb.net/sih-tracking?retryWrites=true&w=majority&appName=Cluster0";
-
-// Connect MongoDB
-mongoose.connect(uri)
-    .then(() => console.log("✅ MongoDB Connected"))
-    .catch(err => {
-        console.error("❌ MongoDB Error:", err.message);
-
-    });
-
-// Middleware
 app.use(express.json());
 
-// Example route
+// MongoDB Atlas connection
+const uri = "mongodb+srv://bora:arnav@cluster0.viblu0y.mongodb.net/sih-tracking?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(uri, {
 
+})
+    .then(() => console.log("✅ MongoDB Connected"))
+    .catch(err => console.error("❌ MongoDB Error:", err));
+
+// Use driver routes
 app.use("/api/driver", driverRoutes);
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
 
-// Start server
-const PORT =  5000;
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-});
+const PORT = 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
